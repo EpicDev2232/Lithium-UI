5a3c5d3f4138131d4ee32c3e5097608267761298
EpicDev2232 <79506218+EpicDev2232@users.noreply.github.com>
2023-04-16 15:08:06 +0100

    Update Lithium-Nightly (UNSTABLE).lua

--- Lithium-Nightly (UNSTABLE).lua
+++ Lithium-Nightly (UNSTABLE).lua
@@ -1683,7 +1683,7 @@ function Lithium:MakeWindow(WindowProperties)
 		end
 
 		for _, file in pairs(listfiles(savename)) do
-			Settings.PresetList:AddElement(file)
+			Settings.PresetList:AddElement(string.sub(file, #savename, #file - 4))
 		end
 		
 		return Settings
