4eae40e5ebe9225a6197ed20aea7d73390db7acd
EpicDev2232 <79506218+EpicDev2232@users.noreply.github.com>
2023-10-04 19:43:34 +0100

    Update Lithium-Nightly (UNSTABLE).lua

--- Lithium-Nightly (UNSTABLE).lua
+++ Lithium-Nightly (UNSTABLE).lua
@@ -132,10 +132,18 @@ function Lithium:MakeWindow(WindowProperties)
 	-- code
 
 	UserInputService.InputBegan:Connect(function(input, gameProcessed)
+		print(input.KeyCode, input.KeyCode == Enum.KeyCode.RightShift)
+			
 		if input.KeyCode == Enum.KeyCode.RightShift then
+			print("j")
+				
 			Window.Enabled = not Window.Enabled
 
+			print("caraguer")
+
 			LithiumContainer.Enabled = Window.Enabled
+
+			print(LithiumContainer.Enabled , Window.Enabled)
 		elseif input.UserInputType == Enum.UserInputType.MouseButton1 then
 			local mousePosition = UserInputService:GetMouseLocation() - Vector2.new(0, 30)
 
